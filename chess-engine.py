025ca601ad061d138bbe23fe4e31f66e6bf8a777
Vicente <vt.bmro@gmail.com>
2023-11-17 13:35:34 +0100

    Initial commit

---
+++ chess-engine.py
@@ -0,0 +1,18 @@
+
+'''
+My idea is to combine both my final project of CS50x and CS50w by building an
+engine in python and the building a interactive web page where users can use 
+the engine to analyze postions
+'''
+
+
+'''
+
+1. A engine comes with two functions: a Search function and an Evaluate function, 
+While evaluating, a chess engine looks at all pieces and ascertains the position 
+that would be better for each. During this process, all chess engines display 
+something called an eval.
+
+'''
+
+# TODO: Write a function that takes as input a chess board, 
