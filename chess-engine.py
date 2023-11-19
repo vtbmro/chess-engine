5e2c6412399c09114ab506110a65272801a391c5
Vicente <vt.bmro@gmail.com>
2023-11-19 18:36:16 +0100

    iterates trough board correctly

--- chess-engine.py
+++ chess-engine.py
@@ -26,14 +26,30 @@ integer is the position is favorable for white or a negative integer
 if the position is favorable for black (or near 0 if equal)
 """
 
-
+# Added this to be able to traverse trough the board easily
+algebraic_notation = [
+'a1', 'b1', 'c1', 'd1', 'e1', 'f1', 'g1', 'h1',
+'a2', 'b2', 'c2', 'd2', 'e2', 'f2', 'g2', 'h2',
+'a3', 'b3', 'c3', 'd3', 'e3', 'f3', 'g3', 'h3',
+'a4', 'b4', 'c4', 'd4', 'e4', 'f4', 'g4', 'h4',
+'a5', 'b5', 'c5', 'd5', 'e5', 'f5', 'g5', 'h5',
+'a6', 'b6', 'c6', 'd6', 'e6', 'f6', 'g6', 'h6',
+'a7', 'b7', 'c7', 'd7', 'e7', 'f7', 'g7', 'h7',
+'a8', 'b8', 'c8', 'd8', 'e8', 'f8', 'g8', 'h8'
+]
 
 def evaluate(board):
     position = 0
-    # Iterate trough the board
 
-    # If piece white add positive integer 
+# Correctly iterates trough the board
+    for square in algebraic_notation:
+        piece = board.piece_at(chess.parse_square(f"{square}"))
+        print(piece)
+# White piece: add positive value to position acording to piece
 
-    # 
+# Black piece: add negative value to position according to piece
+
 
     return position
+
+evaluate(board)
