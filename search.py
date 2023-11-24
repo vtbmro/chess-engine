4f6396d15336ef206e9146a1c92544dfc0095d5b
Vicente <vt.bmro@gmail.com>
2023-11-24 16:15:47 +0100

    minimax algorith to find best move

--- search.py
+++ search.py
@@ -1,6 +1,32 @@
 import chess
 import eval
+from eval import evaluate
+
 
 """
-Create function that navigates trough all the 
 """
+
+def minimax(node, depth, isMaximizingPlayer, alpha, beta):
+    if depth == 0 or game.isover() == true:
+        return evaluate(node)
+
+    # White 
+    if isMaximizingPlayer:
+        bestVal = -10000 
+        for move in legalmoves:
+            value = minimax(node, depth+1, false, alpha, beta)
+            bestVal = max( bestVal, value) 
+            alpha = max( alpha, bestVal)
+            if beta <= alpha:
+                break
+        return bestVal
+
+    else:
+        bestVal = 1000
+        for move in legalmoves:
+            value = minimax(node, depth+1, true, alpha, beta)
+            bestVal = min( bestVal, value) 
+            beta = min( beta, bestVal)
+            if beta <= alpha:
+                break
+        return bestVal
