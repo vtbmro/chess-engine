bfe09c02f2724ef58cb857ef52bdd3075ac07eea
Vicente <vt.bmro@gmail.com>
2023-12-04 16:14:04 +0100

    added an example to illustrate the functionality of the program

---
+++ example.py
@@ -0,0 +1,17 @@
+import chess
+import copy
+import math
+from eval import evaluate
+from search import minimax_alphabeta
+
+board = chess.Board("8/8/6pr/6p1/5pPk/5P1p/5P1K/R7")
+
+best_value, best_moves = minimax_alphabeta(board, 5, -math.inf, math.inf, True)
+
+
+for move in best_moves:
+    board.push_san(f"{move}")
+    print(board)
+    print("")
+
+print(best_value)
